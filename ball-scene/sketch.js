b3f47254013bf9a5508a8b1dfb0fac229fd7a13b
PeterSparks-lab <71788421+PeterSparks-lab@users.noreply.github.com>
2021-09-20 10:59:48 -0600

    updated ball scene

--- ball-scene/sketch.js
+++ ball-scene/sketch.js
@@ -5,23 +5,37 @@ let ballArray = [];
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  for (let index=0; index< 30; index++){
+  for (let index = 0; index < 30; index++){
+    spawnBall();
+  }
+
+  //spawn a ball every 0.5 seconds
+  window.setInterval(spawnBall, 500);
+
+}
+
+function spawnBall(){
     let newBall = {
     x: random(width),
     y: random(height),
      radius: random(5,20),
-    dx: random(-6,7),
-    dy: random(-6,7),
+    dx: random(-5,5),
+    dy: random(-5,5),
     color: ('#006989'),
-    }
+    };
     ballArray.push(newBall);
-}
 }
 
 function draw() {
   background(220);
   moveBall();
   displayBall();
+}
+
+function mousePressed(){
+  spawnBall();
+  ballArray[ballArray.length - 1].x = mouseX;
+  ballArray[ballArray.length - 1].y = mouseY;
 }
 
 function moveBall(){
