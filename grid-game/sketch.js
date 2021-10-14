3c85bff4e5ee1f4c7596d56fbed97314106ed712
PeterSparks-lab <71788421+PeterSparks-lab@users.noreply.github.com>
2021-10-13 18:44:41 -0600

    cleaned code, removed unused functions and variables

--- grid-game/sketch.js
+++ grid-game/sketch.js
@@ -1,15 +1,15 @@
-// Project Title
-// Your Name
+// Grid Based Game
+// Peter Sparks
 // Date
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// - used multiple grids
+// - used objects
+// - used text file to create level
 
-// Declare global variables
-let spawnBox = [];
+// Declare global variables.....................................................................................................................................//
 let gameArray;
 let inventory;
-let area;
 let spaceSize = 23;
 let inventoryY = 3;
 let inventoryX = 7;
@@ -27,24 +27,21 @@ let playerX;
 let playerY;
 let grid1;
 let grid2;
-let invBack;
 let yBox;
 let gBox;
-let box1;
-let box2;
+let yellowBox;
+let greyBox;
 let invBox1;
 let invBox2;
 let score = 0;
 let waitTime = 5000;
 let startTime;
-// console.log("globals");
+//..............................................................................................................................................................//
 
 
-//Load map and sprites from assets folder
+//Load map and sprites from assets folder before setup.......................................................................................................................//
 function preload(){
-  // console.log("preload");
   grid2 = loadStrings("assets/levels/level.txt");
-  //grid1 = loadStrings("assets/levels/level3.txt");
   img = loadImage("assets/images/warehouse-2.png.png");
   right = loadImage("assets/images/guyFaceRight.png");
   left = loadImage("assets/images/guyFaceLeft.png");
@@ -54,16 +51,14 @@ function preload(){
   gBox = loadImage("assets/images/greybox.png");
   invBox1 = loadImage("assets/images/yBox2.png");
   invBox2 = loadImage("assets/images/greybox1.png");
-  box1 = new Box(3,19,yBox,2000);
-  box2 = new Box(3,20,gBox,3500);
-
-  //invBack = loadImage("assets/images/invBackgound.png");
+  yellowBox = new Box(3,19,yBox,2000);
+  greyBox = new Box(3,20,gBox,3500);
   guy = right;
 }
+//..............................................................................................................................................................//
 
+//..............................................................................................................................................................//
 function setup() {
-  // console.log("setup");
-  //createCanvas(windowWidth,windowHeight);
   createCanvas(450,450);
   grid1 = [
     [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22],
@@ -94,36 +89,29 @@ function setup() {
   inventory = createInventory(inventoryY, inventoryX);
   slotSize = width/7;
   spaces = width/spaceSize;
-  area = theGameArea();
   playerY = grid1[1][1];
   playerX = grid1[1][1];
   console.log(playerX,playerY);
   startTime = millis();
 }
+//..............................................................................................................................................................//
 
-
+//..............................................................................................................................................................//
 function draw() {
-  // console.log("draw");
   background(img);
-  //image(guy,25,25,50,50);
   player();
-  theGuy();
   if (showInv) {
     displayInventory();
-    
   }
-  if (!showInv){
-    theGameArea();
-
-  }
-  box1.display();
-  box1.move();
-  box2.display();
-  box2.move();
-  theText();
+  yellowBox.display();
+  yellowBox.move();
+  greyBox.display();
+  greyBox.move();
+  theScore();
 }
+//..............................................................................................................................................................//
 
-//Creates the array used for the inventory
+//Creates the array used for the inventory......................................................................................................................//
 function createInventory(invHeight,invWidth) {
   invArray = [];
   for (let y=0; y<invHeight; y++) {
@@ -136,96 +124,43 @@ function createInventory(invHeight,invWidth) {
   //console.log(invArray);
   return invArray;
 }
+//..............................................................................................................................................................//
 
-function keyPressed() {
-  if (key === "q") {
-    showInv = !showInv;
-  }
-  if (key === "d") {
-    guy = right;
-    if (grid2[playerY][playerX+1] === "."){
-      playerX += 1;
-      console.log(playerX);
-    }
-  }
-  if (key === "a") {
-    guy = left;
-    if (grid2[playerY][playerX-1] === ".") {
-      playerX -=1;
-      console.log(playerX);
-    }
+//Handles input from keys.......................................................................................................................................//
+function keyTyped() {
+  if (key === "s") {
+    moveDown();
   }
   if (key === "w") {
-    guy = back;
-    if (grid2[playerY-1][playerX] === ".") {
-      playerY -= 1;
-      console.log(playerY);
-    }
+    moveUp();
   }
-  if (key === "s") {
-    guy = front;
-    if (grid2[playerY+1][playerX] === ".") {
-      playerY += 1;
-      console.log(playerY);
-    }
+  if (key === "a") {
+    moveLeft();
   }
-  if (key === " ") {
-    for (let y=0; y<inventoryY; y++) {
-      for (let x=0; x<inventoryX; x++) {
-        if (inventory[y][x] === 0) {
-          if (guy === front) {
-            if (box1.x === playerX && box1.y === playerY+1) {
-              if (playerX !== 3) {
-                box1.onConveyor = false;
-                inventory[y][x] = 1;
-                box1.reset();
-              }
-            }
-            else if (box2.x === playerX && box2.y === playerY+2) {
-              if (playerX !== 3) {
-                box2.onConveyor = false;
-                inventory[y][x] = 2;
-                box2.reset();
-              }
-            }
-          }
-        }
-      }
-    }
+  if (key === "d") {
+    moveRight();
+  }
+  if (key === "q") {
+    toggleInventory();
   }
   if (key === "e") {
     if (guy === right) {
-      if (grid2[playerY-1][playerX+1] === "Y") {
-        showInv = true;
-        for (let y=0; y<inventoryY; y++) {
-          for (let x=0; x<inventoryX; x++) {
-            if (inventory[y][x] === 1) {
-              inventory[y][x] = 0;
-              score += 10;
-            }
-          }
-        }
-      }
-    }
-    if (guy === right) {
       if (grid2[playerY-1][playerX+1] === "G") {
-        showInv = true;
-        for (let y=0; y<inventoryY; y++) {
-          for (let x=0; x<inventoryX; x++) {
-            if (inventory[y][x] === 2) {
-              inventory[y][x] = 0;
-              score += 100;
-            }
-          }
-        }
+        depositGreys();
+      }
+      if (grid2[playerY-1][playerX+1] === "Y") {
+        depositYellows();
       }
     }
   }
+  if (key === " ") {
+    pickupBoxes();
+  }
 }
+//..............................................................................................................................................................//
 
-
+//Draws the inventory on the canvas.............................................................................................................................//
 function displayInventory() {
-  
   for (let y=0; y<inventoryY; y++) {
     for (let x=0; x<inventoryX; x++) {
       if (inventory[y][x] === 1) {
@@ -242,54 +177,43 @@ function displayInventory() {
     }
   }
 }
+//..............................................................................................................................................................//
 
-
-function theGameArea() {
-  gameArray = [];
-  for (let y=0; y<spaceSize; y++) {
-    for (let x=0; x<spaceSize; x++) {
-      noFill();
-      noStroke();
-      rect(x*spaces, y*spaces, spaces, spaces);
-    }
-  }
-} 
-
+//..............................................................................................................................................................//
 function player(){
   noStroke();
   noFill();
-  //fill("black");
-  rect(playerX*spaces,playerY*spaces,spaces,spaces);
-}
-
-function theGuy(){
   image(guy,playerX*spaces,playerY*spaces-spaces, spaces, spaces*2);
+  //rect(playerX*spaces,playerY*spaces,spaces,spaces);
 }
+//..............................................................................................................................................................//
 
+//Creates a class that is used to generate the boxes as objects.................................................................................................//
 class Box {
+  //Consructs the object.......................................................//
   constructor(x,y,sprite,duration) {
     this.x = x;
     this.alsoX = x;
     this.y = y;
     this.sprite = sprite;
-    //this.size = spaces;
     this.onConveyor = true;
     this.collected = false;
     this.duration = duration;
     this.lastMove = 0;
   }
+  //...........................................................................//
 
-
+  //displays the object........................................................//
   display() {
     noFill();
     noStroke();
     if (this.onConveyor === true) {
       image(this.sprite,this.x*spaces, this.y*spaces, spaces, spaces);
-
     }
-    
   }
+  //...........................................................................//
 
+  //Moves the object on the conveyor...........................................//
   move() {
     if (this.onConveyor === true) {
       if (this.x === 11) {
@@ -302,17 +226,122 @@ class Box {
       }
     }
   }
+  //...........................................................................//
 
+  //Resets the position of the object..........................................//
   reset() {
     this.x = this.alsoX;
     console.log(millis());
     this.lastMove = millis() + this.duration;
     this.onConveyor = true;
   }
+  //...........................................................................//
 
 }
+//..............................................................................................................................................................//
 
-function theText() {
+//Displays the score onscreen...................................................................................................................................//
+function theScore() {
   stroke(1);
   text("Score: "+score, 50,250);
 }
+//..............................................................................................................................................................//
+
+//Toggles the inventory.......................................................................................................................................................//
+function toggleInventory(){
+  showInv = !showInv;
+}
+//..............................................................................................................................................................//
+
+//Moves the player one space to the right.......................................................................................................................//
+function moveRight() {
+  guy = right;
+  if (grid2[playerY][playerX+1] === "."){
+    playerX += 1;
+    //console.log("X = " + playerX);
+  }
+}
+//..............................................................................................................................................................//
+
+//Moves the player one space to the left........................................................................................................................//
+function moveLeft() {
+  guy = left;
+  if (grid2[playerY][playerX-1] === ".") {
+    playerX -=1;
+    //console.log("X = " + playerX);
+  }
+}
+//..............................................................................................................................................................//
+
+//Moves the player up one space.................................................................................................................................//
+function moveUp() {
+  guy = back;
+  if (grid2[playerY-1][playerX] === ".") {
+    playerY -= 1;
+    //console.log("Y = " + playerY);
+  }
+}
+//..............................................................................................................................................................//
+
+//Moves the player down one space...............................................................................................................................//
+function moveDown() {
+  guy = front;
+  if (grid2[playerY+1][playerX] === ".") {
+    playerY += 1;
+    //console.log("Y = " + playerY);
+  }
+}
+//..............................................................................................................................................................//
+
+//Checks for an empty slot in the inventory, if one is found a box is placed into that slot.....................................................................//
+function pickupBoxes() {
+  for (let y=0; y<inventoryY; y++) {
+    for (let x=0; x<inventoryX; x++) {
+      if (inventory[y][x] === 0) {
+        if (guy === front) {
+          if (yellowBox.x === playerX && yellowBox.y === playerY+1) {
+            if (playerX !== 3) {
+              yellowBox.onConveyor = false;
+              inventory[y][x] = 1;
+              yellowBox.reset();
+            }
+          }
+          else if (greyBox.x === playerX && greyBox.y === playerY+2) {
+            if (playerX !== 3) {
+              greyBox.onConveyor = false;
+              inventory[y][x] = 2;
+              greyBox.reset();
+            }
+          }
+        }
+      }
+    }
+  }
+}
+//..............................................................................................................................................................//
+
+//Removes all grey boxes from the inventory and increases the score for each box added..........................................................................//
+function depositGreys() {
+  for (let y=0; y<inventoryY; y++) {
+    for (let x=0; x<inventoryX; x++) {
+        if (inventory[y][x] === 2) {
+        inventory[y][x] = 0;
+        score += 100;
+      }
+    }
+  }
+}
+//..............................................................................................................................................................//
+
+//Removes all yellow boxes from the inventory and increases the score for each box added........................................................................//
+function depositYellows() {
+  for (let y=0; y<inventoryY; y++) {
+    for (let x=0; x<inventoryX; x++) {
+      if (inventory[y][x] === 1) {
+        inventory[y][x] = 0;
+        score += 10;
+      }
+    }
+  }
+}
+//..............................................................................................................................................................//
